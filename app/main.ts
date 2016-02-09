c111879a4db11d52de033f6d2ee7ec0319fee76b
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-02-09 21:55:06 +0200

    update

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+import {bootstrap} from "angular2/platform/browser";
+import {Component} from "angular2/core";
+
+@Component({
+  selector: 'app',
+  template: `<div>Hello World!</div>`
+})
+
+class App {}
+
+bootstrap(App);
